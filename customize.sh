5014ec57ec398988c7d20852d34a5692ed9bf722
akirasup3r <vishal.rockstar7011@gmail.com>
2022-09-27 03:10:27 +0530

    fix mod installation issue
    Signed-off-by: akirasup3r <vishal.rockstar7011@gmail.com>

--- customize.sh
+++ customize.sh
@@ -1,21 +1,21 @@
 SKIPUNZIP=1
 DEVICE=$(getprop ro.product.mod_device 2>/dev/null)
 ANDROID=$(getprop ro.system.build.version.release 2>/dev/null)
-ui_print ""
 ui_print " ╭━━┳╮╭╮╱╱╱╱╭━━╮ "
 ui_print " ┃╭╮┃┣╋╋┳┳━╮┃━━╋┳┳━┳━┳┳╮ "
 ui_print " ┃┣┫┃━┫┃╭┫╋╰╋━━┃┃┃╋┃┻┫╭╯ "
 ui_print " ╰╯╰┻┻┻┻╯╰━━┻━━┻━┫╭┻━┻╯ "
 ui_print " ╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╱╰╯ "
-ui_print ""
-ui_print "• $DEVICE $ANDROID"
-ui_print ""
+ui_print "*******************************"
+ui_print "- $DEVICE $ANDROID"
 set -x
 if [ "$DEVICE" == "munch_xiaomieu_global" ]; then
     ui_print "- Extracting module files"
     unzip -o "$ZIPFILE" module.prop -d $MODPATH >&2
     unzip -o "$ZIPFILE" service.sh -d $MODPATH >&2
-    [ -f "$MODPATH/system.tar.xz" ] && tar -xf $MODPATH/system.tar.xz -C $MODPATH 2>/dev/null
+    unzip -o "$ZIPFILE" system.tar.xz -d $MODPATH >&2
+    tar -xf $MODPATH/system.tar.xz -C $MODPATH 2>/dev/null
+    rm -rf $MODPATH/system.tar.xz 2>/dev/null
 else
     abort
 fi
